f105daafd9f73ed333c4e02dbe4d22b04db5fe81
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-09 20:21:14 +0530

    DataTypes & Strings

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ fullName = 100;
 console.log(typeof fullName); // Prints type of Data Stored in Variable fullName
 
 // Data Types:
-// String
-// Number
+// String: "Sayeed" '1111111' `Sayeed@1234`
+// Number: 1, 0.1, -3
+// Boolean: true false
+// Arrays: [1, 2,3,4,5,5]
+// Objects: {name: "Ahmed", class: "MERN", passed: true}
+// Functions: function add (a, b) { return a + b; }
---
+++ stringsNumbers.js
@@ -0,0 +1,15 @@
+// var str = "MERN STACK";
+// str = str + " DEVELOPMENT"; // Concatenation
+// console.log(str.length);
+
+// var name = "Syed";
+// ["S", "y", "e", "d"];
+
+// Arithemtic
+// var a = 20;
+// var b = 20;
+// var result = a % b; // Modulus Operator -> Gives Remainder of a division
+// console.log(result);
+
+var a = 10;
+console.log(a % 2);
