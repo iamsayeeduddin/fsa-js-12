951c04fee9eecaca7ffc0cde9b56882abcbffbfd
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-17 20:35:17 +0530

    Switch & Functions

---
+++ functions.js
@@ -0,0 +1,28 @@
+// Functions
+function add(a, b) {
+  if (false) {
+    return a + b;
+  }
+  console.log("OJNLFSNLNDSNDSNDKJKNJ");
+  console.log("OJNLFSNLNDSNDSNDKJKNJ");
+  console.log("OJNLFSNLNDSNDSNDKJKNJ");
+  console.log("OJNLFSNLNDSNDSNDKJKNJ");
+  console.log("OJNLFSNLNDSNDSNDKJKNJ");
+  console.log("OJNLFSNLNDSNDSNDKJKNJ");
+  //   return undefined; Internally added by JS if return not executed bfore the end of function
+}
+
+var subtract = function (a, b) {
+  return a - b;
+};
+
+var x = add(1, 2); // 2
+var y = add(10, 2); // 12
+console.log(x, y);
+
+var z = subtract(20, 10);
+console.log(z);
+
+function isValidEmail(email) {
+  return true || false;
+}
--- operators.js
+++ operators.js
@@ -70,8 +70,47 @@ var x = 22;
 var age = 17;
 var isLicenseAvailable = false;
 
-if (age > 17 && isLicenseAvailable) {
-  console.log("Allowed to Drive on Charminar!");
+// if (age > 17 && isLicenseAvailable) {
+//   console.log("Allowed to Drive on Charminar!");
+// } else {
+//   console.log("Galli mai chala lo!");
+// }
+
+// EMAIL VALIDATION USING STRING METHODS, OPERATORS AND IF ELSE STATEMENTS
+
+var email = "sayeed @@gmail.com";
+var isValidEmail = false;
+
+// contains @
+// must not start with @
+// must not end with @ and .
+// . come after @iamsayeeduddin
+// no repetition of @
+// . must not come right after @
+// " " no empty spaces
+
+if (!email.includes("@")) {
+  console.log("Invalid Email address 1");
 } else {
-  console.log("Galli mai chala lo!");
+  if (email.startsWith("@")) {
+    console.log("Invalid Email address 2");
+  } else {
+    if (email.endsWith("@") || email.endsWith(".")) {
+      console.log("Invalid Email address 3");
+    } else {
+      if (email.indexOf("@") + 1 < email.indexOf(".") && email.indexOf("@") === email.lastIndexOf("@")) {
+        console.log("Email Address is valid!");
+      } else {
+        console.log("Invalid Email Address! 4");
+      }
+    }
+  }
 }
+
+// REGEX
+
+// if (email.test(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)) {
+//   console.log("Valid Email Address");
+// } else {
+//   console.log("Invalid Email");
+// }
