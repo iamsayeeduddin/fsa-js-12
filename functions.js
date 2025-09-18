14bc52b41f63b7664073fbb9b96b6a5a177f9853
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-18 20:21:21 +0530

    Functions & Arrays

--- functions.js
+++ functions.js
@@ -16,13 +16,31 @@ var subtract = function (a, b) {
   return a - b;
 };
 
-var x = add(1, 2); // 2
-var y = add(10, 2); // 12
-console.log(x, y);
+// var x = add(1, 2); // 2
+// var y = add(10, 2); // 12
+// console.log(x, y);
 
-var z = subtract(20, 10);
-console.log(z);
+// var z = subtract(20, 10);
+// console.log(z);
 
 function isValidEmail(email) {
-  return true || false;
+  if (
+    email.includes("@") &&
+    !email.startsWith("@") &&
+    !(email.endsWith("@") || email.endsWith(".")) &&
+    email.indexOf("@") + 1 < email.indexOf(".") &&
+    email.indexOf("@") === email.lastIndexOf("@") &&
+    !email.includes(" ")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let res = isValidEmail("sayeed@gmail.com.sa");
+if (!res) {
+  console.log("Invalid Email");
+} else {
+  console.log("Valid Email");
 }
