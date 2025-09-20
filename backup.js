d35e83c2861b0fc6f8f9a9d8dd5221a55fa2f8e2
Sayeeduddin <iamsayeeduddin@outlook.com>
2025-09-20 15:11:34 +0530

    Backup Session

---
+++ backup.js
@@ -0,0 +1,37 @@
+/// JS - JavaScript -Add Functionality Website - Web 2.0
+// Its a Loosely-Typed Dynamic Interpreted Programming Language
+
+// Variables  -  A Special Container created to store a value
+var variableName; // Variable Declaration
+variableName = "Some Value";
+
+// console.log(variableName);
+
+variableName = "Other Value";
+
+// console.log(typeof variableName);
+
+variableName = 10;
+
+// console.log(typeof variableName);
+
+// DataTypes - The type of value stored or written in JS.
+// String - "123456789", '*&^*&#^@*&^', `Sayeed1234`, "Name"
+// Number - 12344, 0.001, -1
+// Boolean - true, false
+// Arrays - [] - [1,3,4,4,4,5,5,6 false, {}, add()] - [1,"2",4,5,5]
+// Objects - {} - {name: "Sayeed", isActive: false, phone: 9874563210}
+// functions - function(){} - Reusable Block of Code -> React
+// undefined
+// BigInt
+// Symbol
+// Date
+
+// String
+var str = "A String to test JS. Again test.";
+
+// console.log(str.slice(1, 4));\
+// console.log(str.indexOf("test"));
+// console.log(str.endsWith("Test."));
+
+console.log(Boolean(" "));
